5cd68da81a3d63378ef84a2d99f11867793fe6e7
Gunnar Knutson <gknutson1@ewu.edu>
2023-05-07 13:14:25 -0700

    Misc. minor tweaks

--- SpotList.py
+++ SpotList.py
@@ -15,6 +15,7 @@ app = FastAPI(
         version="v0.0.1"
         )
 
+
 @app.exception_handler(AuthorizationException)
 async def authorization_exception_handler(request, exception: AuthorizationException):
     return JSONResponse('username or password incorrect', status.HTTP_401_UNAUTHORIZED)
@@ -22,7 +23,9 @@ async def authorization_exception_handler(request, exception: AuthorizationExcep
 
 @app.exception_handler(requests.HTTPError)
 async def http_exception_handler(request, exception: requests.HTTPError):
-    return JSONResponse('encountered an error when communicating with the Spotify API', status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return JSONResponse(
+            'encountered an error when communicating with the Spotify API',
+            status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @app.post("/new", status_code=status.HTTP_201_CREATED)
@@ -51,12 +54,12 @@ async def build_playlist(user_id: Annotated[str, Header()], token: Annotated[str
 async def get_auth_link():
     # Use our credentials to get the authorization url from Spotify
     parameters = {"response_type": "code", "client_id": cfg.client_id,
-                  "redirect_uri": f"{cfg.redirect_uri}",
-                  "scope": "playlist-read-private playlist-modify-private playlist-modify-public",
+                  "redirect_uri" : f"{cfg.redirect_uri}",
+                  "scope"        : "playlist-read-private playlist-modify-private playlist-modify-public",
                   # 'state' will be passed back to us after the user logs in, allowing us to identify what client
                   # the user belongs to. The user will use this to authenticate to SpotList as well.
-                  "state": uuid.uuid4().hex,
-                  "show_dialog": "true"}
+                  "state"        : uuid.uuid4().hex,
+                  "show_dialog"  : "true"}
     request = requests.get(f"https://accounts.spotify.com/authorize", params=parameters)
     request.raise_for_status()
     # Send the URL back to the user. All further requests from the user will need the token.
@@ -64,12 +67,12 @@ async def get_auth_link():
 
 
 # Once the user has authenticated with Spotify, they will be redirected here with their authorization code.
-@app.get("/callback", status_code=200)
+@app.get("/callback", status_code=status.HTTP_200_OK)
 async def get_token(code: str, state: str):
     # Exchange the temporary authorization code for (semi-)permanent authorization credentials
     headers = {"Content-Type": "application/x-www-form-urlencoded", "Authorization": cfg.auth_header}
-    parameters = {"grant_type": "authorization_code",
-                  "code": code,
+    parameters = {"grant_type"  : "authorization_code",
+                  "code"        : code,
                   "redirect_uri": f"{cfg.redirect_uri}"}
     request = requests.post(f"{cfg.auth_url}/api/token", params=parameters, headers=headers)
     request.raise_for_status()
@@ -81,7 +84,8 @@ async def get_token(code: str, state: str):
     request.raise_for_status()
     user_data = request.json()
 
-    cfg.db.execute(f"""
+    cfg.db.execute(
+            f"""
         INSERT INTO users
             (spotify_id, 
              display_name,
@@ -96,7 +100,8 @@ async def get_token(code: str, state: str):
              '{user_auth['refresh_token']}', 
              '{user_auth['expires_in'] + time.time()}',
              '{state}')
-        """)
+        """
+            )
 
     cfg.db.commit()
 
